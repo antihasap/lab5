c4b0e5da85d8e198111bbebb9b60dbb119ab2904
antihasap <124263660+antihasap@users.noreply.github.com>
2025-12-12 23:28:27 +0300

    Update lab5.h

--- lab5.h
+++ lab5.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 typedef struct Flight {
     int id;
@@ -30,15 +31,18 @@ Airport* load_from_file(const char* filename);
 void add_flight(Airport* db, const char* flight_number, const char* plane_name,
                 const char* departure_time, const char* arrival_time);
 void print_flights(Airport* db);
-Flight* find_flight(Airport* db, const char* flight_number);
+Flight* find_flight_by_number(Airport* db, const char* flight_number);
+void find_flights_by_plane(Airport* db, const char* plane_name);
+void find_flights_by_departure_time(Airport* db, const char* departure_time);
+void find_flights_by_arrival_time(Airport* db, const char* arrival_time);
 void delete_flight(Airport* db, int id);
 void edit_flight(Airport* db, int id, char* flight_number, 
                 char* plane_name, char* departure_time,
                 char* arrival_time);
-                
+int validate_time_format(const char* time);                
 void print_details(Flight* flight);
 void clear_input_buffer();
 void print_menu();
-
+void print_search_menu();
 
 #endif
